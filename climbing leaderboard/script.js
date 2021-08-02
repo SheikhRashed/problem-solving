ba722a6e87f79569a3745fcfa1c013d1a3d608c0
Sheikh Rashed <mdrashed1310@gmail.com>
2021-08-02 22:42:14 +0600

    climbing leaderboard solved

---
+++ climbing leaderboard/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function climbingLeaderboard(scores, alice) {
+  let rank = [];
+  for (let i = 0; i < scores.length; i++) {
+    if (rank.length === 0) {
+      rank.push(scores[i]);
+    } else {
+      if (rank[rank.length - 1] === scores[i]) {
+        continue;
+      }
+      rank.push(scores[i]);
+    }
+  }
+  let res = [];
+  let lowestRank = rank.length;
+  for (let i = 0; i < alice.length; i++) {
+    while (alice[i] >= rank[lowestRank - 1] && lowestRank > 0) {
+      rank.pop;
+      lowestRank--;
+    }
+    res.push(lowestRank + 1);
+  }
+  return res;
+}
+
+const scores = [100, 90, 90, 80, 75, 60];
+const alice = [50, 65, 77, 90, 102];
+
+let output = climbingLeaderboard(scores, alice);
+console.log(output);
