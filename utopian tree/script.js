824af764811ca9abd647353f2eddf5e5ead121ae
Sheikh Rashed <mdrashed1310@gmail.com>
2021-08-04 07:22:14 +0600

    solved utopian tree problem

---
+++ utopian tree/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function utopianTree(n) {
+  let cycle = 1;
+  let height = 1;
+
+  while (cycle <= n) {
+    if (cycle % 2 !== 0) {
+      height *= 2;
+    } else {
+      height++;
+    }
+    cycle++;
+  }
+  return height;
+}
+
+let output = utopianTree(40);
+console.log(output);
